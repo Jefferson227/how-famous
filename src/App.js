0a06533b180407011366ad8bbd7108baae5d338d
Jefferson Matheus <jefferson227@gmail.com>
2019-08-21 18:50:02 -0300

    Add example artist image

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function App() {
       <header className="App-header">
         <input className="Input-search" placeholder="Type an artist name" />
       </header>
+
+      <section>
+        <div>
+          <img
+            className="Artist__image"
+            src="https://i.scdn.co/image/cdc26c67ffe1c377e0521435eb4a46ce0ec23876"
+            alt="Megadeth"
+          />
+        </div>
+      </section>
     </div>
   );
 }
