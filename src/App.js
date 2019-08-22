810b6f8b3cb9069dcceedd6a9be16365e4d4ad70
Jefferson Matheus <jefferson227@gmail.com>
2019-08-22 15:42:32 -0300

    Add hardcoded artists

--- src/App.js
+++ src/App.js
@@ -9,13 +9,41 @@ function App() {
         <input className="Input-search" placeholder="Type an artist name" />
       </header>
 
-      <section>
-        <div>
+      <section className="Artists">
+        <div className="Artist">
           <img
             className="Artist__image"
             src="https://i.scdn.co/image/cdc26c67ffe1c377e0521435eb4a46ce0ec23876"
             alt="Megadeth"
           />
+          <div className="Artist__name">Megadeth</div>
+        </div>
+
+        <div className="Artist">
+          <img
+            className="Artist__image"
+            src="https://i.scdn.co/image/cdc26c67ffe1c377e0521435eb4a46ce0ec23876"
+            alt="Megadeth"
+          />
+          <div className="Artist__name">Megadeth</div>
+        </div>
+
+        <div className="Artist">
+          <img
+            className="Artist__image"
+            src="https://i.scdn.co/image/cdc26c67ffe1c377e0521435eb4a46ce0ec23876"
+            alt="Megadeth"
+          />
+          <div className="Artist__name">Megadeth</div>
+        </div>
+
+        <div className="Artist">
+          <img
+            className="Artist__image"
+            src="https://i.scdn.co/image/cdc26c67ffe1c377e0521435eb4a46ce0ec23876"
+            alt="Megadeth"
+          />
+          <div className="Artist__name">Megadeth</div>
         </div>
       </section>
     </div>
