a58f04d3fb7bef82337894b4bbefdd9b70e8931d
Jefferson Matheus <jefferson227@gmail.com>
2019-08-26 14:50:20 -0300

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Search from "./components/Search/Search";
+import Artist from "./components/Artist/Artist";
 
 class App extends React.Component {
   render() {
-    return <Search />;
+    return (
+      <Router>
+        <Route path="/" component={Search} />
+        <Route path="/artist/:id" component={Artist} />
+      </Router>
+    );
   }
 }
 
--- src/components/Artist/Artist.js
+++ src/components/Artist/Artist.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Artist.css";
 
-const getArtistImage = (imageUrl, name) => {
+const getArtistImage = (imageUrl, name, id) => {
   return imageUrl ? (
-    <img className="Artist__image" src={imageUrl} alt={name} />
+    <Link to={`/artist/${id}`}>
+      <img className="Artist__image" src={imageUrl} alt={name} />
+    </Link>
   ) : (
-    <div className="Artist__image-placeholder">
-      <i className="fab fa-itunes-note Artist__note-icon" />
-    </div>
+    <Link to={`/artist/${id}`}>
+      <div className="Artist__image-placeholder">
+        <i className="fab fa-itunes-note Artist__note-icon" />
+      </div>
+    </Link>
   );
 };
 
 const Artist = props => (
   <div className="Artist">
-    {getArtistImage(props.imageUrl)}
+    {getArtistImage(props.imageUrl, props.name, props.id)}
     <div className="Artist__name">{props.name}</div>
   </div>
 );
