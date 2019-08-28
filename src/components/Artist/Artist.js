6398f30fe95ecd65859569a00815b62345d80624
Jefferson Matheus <jefferson227@gmail.com>
2019-08-28 15:20:27 -0300

    Do a small refactor on Artist component

--- src/components/Artist/Artist.js
+++ src/components/Artist/Artist.js
@@ -16,11 +16,15 @@ const getArtistImage = (imageUrl, name, id) => {
   );
 };
 
-const Artist = props => (
-  <div className="Artist">
-    {getArtistImage(props.artist.image, props.artist.name, props.artist.id)}
-    <div className="Artist__name">{props.artist.name}</div>
-  </div>
-);
+const Artist = props => {
+  const { image, name, id } = props.artist;
+
+  return (
+    <div className="Artist">
+      {getArtistImage(image, name, id)}
+      <div className="Artist__name">{name}</div>
+    </div>
+  );
+};
 
 export default Artist;
